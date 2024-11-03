464019ad3a863aff1a1c8fe93097477a0cb28f20
siphokazi <naledimuhauli@gmail.com>
2024-11-03 23:34:15 +0200

    added more information

--- client/src/registration.js
+++ client/src/registration.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 import coverPhoto from './images/Image.png';
 import logo from './images/Logo.png';
 import glogin from './images/Glogin.png';
@@ -26,19 +27,23 @@ function registration() {
             <img src={serperator} alt="seperator" />
             <form>
               <div className="mb-3">
-                <label for="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                <input type="name" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='name' />
               </div>
               <div className="mb-3">
-                <label for="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" />
+
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='email' />
+              </div>
+              <div className="mb-3">
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder='password' />
               </div>
               <div className="mb-3 form-check">
                 <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                <label className="form-check-label" for="exampleCheck1">Remember Me</label>
               </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
+              <button type="submit" className="btn btn-primary">Register</button>
+              <p className="account">
+                Already have an account? <div className="login">Log in</div>
+              </p>
             </form>
           </div>
           <div className='col-6'>
